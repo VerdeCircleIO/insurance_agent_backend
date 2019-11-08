29daeb95eb5bad69fcb08608f9a363d4b1bb13ed
Ravi Shanker Tiwari <ravi.rs.tiwari@gmail.com>
2019-11-09 04:50:41 +0530

    Adding cors fixes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
+
 from modules.api import api
 
 app = Flask(__name__)
+CORS(app, resources='{"/api/*": {"origins": "*"}')
 app.register_blueprint(api, url_prefix="/api")
 
 if __name__ == '__main__':
